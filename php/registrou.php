6c648075f2aece524ffb1de57b593f94c5b327d0
grupoccima1 <111792599+grupoccima1@users.noreply.github.com>
2023-07-07 17:39:45 -0600

    cambios-7

--- php/registrou.php
+++ php/registrou.php
@@ -3,66 +3,93 @@ require_once "./../php/connect.php";
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registro</title>
-
+    <title>CCIMAIT | Registrar</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://kit.fontawesome.com/fcdf70aeb7.js" crossorigin="anonymous"></script>
-
-        <link rel="stylesheet" href="../css/estilo.css">
+    <link rel="stylesheet" href="../css/estilo.css">
+    <link rel="shortcut icon" type="image/x-icon" href="../img/icons/logo-icon.ico">
 </head>
-<body>
-    <form class="formulario" action="./registro.php" method="post">
 
-        <h1>Registrate</h1>
-        <div class="contenedor">
-
-
-            <div class="input-contenedor" >
-                <i class="fa-solid fa-user icon"></i>
-                <input type="text" id="nombre" name="nombre" placeholder="Nombre" required>
+<body class="bg-img">
+    <div class="container">
+        <div class="row justify-content-between align-content-center h100">
+            <div class="col-4 text-white d-flex flex-column justify-content-center">
+                <h1 class="text-white text-shadow-logo">CCIMA IT</h1>
+                <hr class="border border-white">
+                <span class="fs-5"> Estas listo para Empezar</span>
+                <div class="mt-5">
+                    <p class="fs-5 text-start">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus
+                        necessitatibus omnis </p>
+                    <a class="btn btn-primary bg-trasparent text-shadow mt-3 px-5 fw-bold" href="">Soporte Tecnico</a>
+                </div>
             </div>
+            <div class="col-4 bg-gassdoor">
+                <div class="mt-3">
+                    <h3 class="text-white text-uppercase text-center pb-3">Registrar</h3>
+                    <!-- Formulario de Registro -->
+                    <form action="./registro.php" method="post" class="m-3">
+                        <div class="mb-3 mt-3">
+                            <label for="usuario" class="text-white form-label">Usuario:</label>
+                            <input class="form-control" type="text" id="nombre" name="nombre" placeholder="Nombre" required>
+                        </div>
+                        <div class="mb-3 mt-3">
+                            <label for="usuario" class="text-white form-label">Usuario:</label>
+                            <input class="form-control" id="usuario" name="usuario" placeholder="Usuario" required>
+                        </div>
+                        <div class="mb-3 mt-3">
+                            <label for="usuario" class="text-white form-label">Usuario:</label>
+                            <input class="form-control" id="telefono" name="telefono" placeholder="Telefono" required>
+                        </div>
+                        <div class="mb-3 mt-3">
+                            <label for="usuario" class="text-white form-label">Usuario:</label>
+                            <input class="form-control" type="text"  id="correo" name="correo" placeholder="Correo" required>
+                        </div>
 
-            <div class="input-contenedor" >
-                <i class="fa-solid fa-user icon"></i>
-                <input type="text" id="usuario" name="usuario" placeholder="Usuario" required>
+                        <div class="mb-3 mt-3">
+                            <label for="area" class="text-white form-label">Area:</label>
+                            <select  class="form-control" id="area" name="area">
+                                <?php $sel="SELECT * FROM area WHERE 1";
+                                            $res=mysqli_query($conexion,$sel);
+                                            while($mos=mysqli_fetch_row($res)){?>
+                                <option class="text-uppercase" value="<?php echo $mos[1]; ?>"> <?php echo $mos[1]; ?> </option>
+                                <?php } ?>
+                            </select>
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="pass" class="text-white form-label">Contraseña:</label>
+                            <input class="form-control" type="password" id="pass" name="password"
+                                placeholder="Password">
+                        </div>
+
+                        <div class="d-flex justify-content-center pt-3">
+                            <button type="submit" class="btn btn-primary bg-pri px-5">Registrase</button>
+                        </div>
+                        <div class="d-flex justify-content-between pt-5">
+                            <!-- Redireccionamiento  -->
+                            <a href="../rpassword.php" class="text-white text-decoration-none">Recuperar
+                                Contraseña</a>
+                            <a href="../index.php" class="text-white text-decoration-none">Login </a>
+                        </div>
+                    </form>
+                </div>
+
             </div>
-
-            <div class="input-contenedor" >
-                <i class="fa-solid fa-user icon"></i>
-                <input type="text" id="telefono" name="telefono" placeholder="Telefono" required>
-            </div>
-
-            <div class="input-contenedor" >
-                <i class="fa-solid fa-user icon"></i>
-                <input type="text" id="correo" name="correo" placeholder="Correo" required>
-            </div>
-            <div class="input-contenedor" >
-                <i class="fa-solid fa-user icon"></i>
-                <select class="op" id="area" name="area">
-                <?php $sel="SELECT * FROM area WHERE 1";
-                                        $res=mysqli_query($conexion,$sel);
-                                        while($mos=mysqli_fetch_row($res)){?>
-                    <option value="<?php echo $mos[1]; ?>"> <?php echo $mos[1]; ?> </option>
-                    <?php } ?> 
-                </select>
-                
-            </div>
-
-
-            <div class="input-contenedor" >
-                <i class="fa-solid fa-key icon"></i>
-                <input type="password" placeholder="Password" id="pass" name="password" required>
-            </div>
-
-            <input type="submit" value="Registrate" class="button">
-            <p>Al registrarte aceptas nuestras condiciones de uso y politica de privacidad</p>
-            <p>¿Ya tienes cuenta?<a href="../index.php">Iniciar sesion</a></p>
         </div>
-    </form>
-    <script src="./../js/validacion.js"></script>
+        <!-- <div class="footer" > 
+            <span class="text-white text-uppercase">todos los derechos reservados <a class="text-white text-decoration-none" href="https://grupoccima.com" target="_blanck">Grupoccima.com</a></span>
+        </div> -->
+    </div>
+
+
+
 </body>
+
 </html>
