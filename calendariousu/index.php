167c94a56ecad5f64104bd136921d2a20c045aba
grupoccima1 <111792599+grupoccima1@users.noreply.github.com>
2023-07-11 18:02:09 -0600

    Add files via upload

--- calendariousu/index.php
+++ calendariousu/index.php
@@ -196,13 +196,20 @@ $mostrar=mysqli_fetch_row($query);
               <tr class="">
                 <td>Nombre:</td>
                 <td><div class="mb-3">
-                  <input type="text" class="form-control" name="" id="txtTitulo" aria-describedby="helpId" disabled value="<?php echo $usuario;?>">
+                  <input type="text"
+                    class="form-control" name="" id="txtTitulo" aria-describedby="helpId" disabled value="
+                    <?php echo $usuario;?>
+                    " >
                 </div></td>
               </tr>
               <tr class="">
                 <td>Area:</td>
                 <td>
-                <input type="text" class="form-control" name="" id="txtArea" aria-describedby="helpId" disabled value="<?php echo $mostrar['0'];?>">
+                <input type="text"
+                    class="form-control" name="" id="txtArea" aria-describedby="helpId" disabled value="
+                    <?php
+                    echo $mostrar['0'];?>
+                    ">
                 </td>
               </tr>
               <tr class="">
@@ -293,7 +300,12 @@ function EnviarInformacion(accion,objEvento){
 }
 
 </script>
-
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+    integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous">
+  </script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"
+    integrity="sha384-fbbOQedDUMZZ5KreZpsbe1LCZPVmfTnH7ois6mU1QK+m14rQ1l2bGBq41eYeM/fS" crossorigin="anonymous">
+  </script>
 </body>
 
 </html>
